0032673c55be36efcd6abc7bfdaf9bdf208a8578
Pavlos Melissinos <pmelissi@iti.gr>
2016-06-30 18:07:25 +0300

    Replace mat file loading with numpy.loadtxt

--- create_label_lmdb.py
+++ create_label_lmdb.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import scipy 
 import scipy.io
 import os.path
-import lmdb                                                    # May require 'pip install lmdb' if lmdb not found
+import lmdb # May require 'pip install lmdb' if lmdb not found
 
 # -------- Import Caffe ---------------
 caffe_root = '~/src/caffe/' 
@@ -25,20 +25,14 @@ dataset = sys.argv[1]
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # Please set the following values and paths as per your needs 
-#N = 1880							# Number of data instances  
-#M = 47								# Number of possible labels for each data instance 
-output_lmdb_path = '/home/pmelissi/repos/dtd/lmdbs/'+dataset+'/label_lmdb'  	# Path of the output label LMDB
-#labels_mat_file = 'labels.mat'					# Mat file for labels N x M 
+output_lmdb_path = '/home/pmelissi/repos/dtd/lmdbs/'+dataset+'/label_lmdb' # Path of the output label LMDB
 input_txt_file = '/home/pmelissi/repos/dtd/Data/labels/' + dataset + '.txt'
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 # -------- Write in LMDB for Caffe ----------
-#X = np.zeros((N, M, 1, 1), dtype=np.uint8)
-#y = np.zeros(N, dtype=np.int64)
-X = np.loadtxt(input_txt_file)
+X = np.loadtxt(input_txt_file,dtype=np.uint8)
 N = X.shape[0]
 M = X.shape[1]
-X = X * 255
 X = X.reshape([N,M,1,1])
 y = np.zeros(N, dtype=np.int64)
 map_size = X.nbytes * 10
@@ -53,8 +47,6 @@ env = lmdb.open(output_lmdb_path, map_size=map_size)
 # To instead check the variable names in the mat file, and use them in a more judicious way, do 
 # array_names = scipy.io.whosmat(labels_mat_file) 	
 # print '\n Array Names \n', array_names
-print X						# Check to see if the contents are well populated within the expected range
-print X.shape					# Check to see if X is of shape N x M x 1 x 1     
 
 with env.begin(write=True) as txn:
     # txn is a Transaction object
@@ -64,6 +56,7 @@ with env.begin(write=True) as txn:
         datum.height = X.shape[2]
         datum.width = X.shape[3]
         datum.data = X[i].tostring()  	# or .tobytes() if numpy < 1.9 
+        raw_input('Press Enter to continue')
         datum.label = int(y[i])
         str_id = '{:08}'.format(i)
 
